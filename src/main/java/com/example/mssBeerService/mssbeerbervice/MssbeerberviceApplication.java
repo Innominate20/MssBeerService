5c7e4bbbc8b7a824d02686e145c06fc3f80ff686
Dave <Dave@gmail.com>
2024-08-15 19:07:13 +0400

    initial commit.

---
+++ src/main/java/com/example/mssBeerService/mssbeerbervice/MssbeerberviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.mssBeerService.mssbeerbervice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MssbeerberviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MssbeerberviceApplication.class, args);
+	}
+
+}
